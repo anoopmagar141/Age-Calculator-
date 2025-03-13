011a268668b1682fceef72f4909d1c8c72f677b3
[Anup  Magar] <am6956275@gmail.com>
2025-03-13 23:32:24 +0545

    Initialize the Person object with birthdate and current date.

--- AgeCalculator.py
+++ AgeCalculator.py
@@ -1,3 +1,7 @@
 from datetime import datetime
  
 class Person:
+    def __init__(self, birthdate):
+        self.birthdate = self._normalize_date(birthdate)
+        self.today = datetime.today()
+   
